dd42c6efad6bddfe47ca06293c58436a90ba8f94
Dim L. Feiron <norefle@outlook.com>
2015-07-30 16:52:29 +0200

    Cleanup

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 organization  := "com.hackathon"
 
-version       := "0.0.1"
+version       := "0.1.1"
 
 scalaVersion  := "2.11.6"
 
--- src/main/scala/com/hackathon/LocalApp.scala
+++ src/main/scala/com/hackathon/LocalApp.scala
@@ -6,6 +6,7 @@ import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
+import scala.tools.nsc.Properties
 
 object LocalApp extends App {
     implicit val system = ActorSystem("lockal-server")
@@ -13,6 +14,7 @@ object LocalApp extends App {
     val service = system.actorOf(Props[LocalServiceActor], "api-service")
 
     implicit val timeout = Timeout(5.seconds)
+    val port = Properties.envOrElse("PORT", "8080").toInt
 
-    IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 8080)
+    IO(Http) ? Http.Bind(service, "0.0.0.0", port)
 }
